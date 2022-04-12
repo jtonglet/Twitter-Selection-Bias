e1b912449ac4c9f87423ce5660c51fbe3812ca53
Jonathan Tonglet <49620076+jtonglet@users.noreply.github.com>
2022-04-12 14:38:01 +0200

    Add files via upload

---
+++ Data_Collection/train_test_split.py
@@ -0,0 +1,18 @@
+#Split train and test set 
+
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+
+active_user_df = pd.read_csv("output/users/active_users/active_user_df_final.csv")
+
+train_df, test_df = train_test_split(active_user_df,
+                                     test_size = 0.02,
+                                     random_state = 42)
+
+
+
+
+train_df.to_csv('output/train_set.csv',index=False)
+test_df.to_csv('output/test_set.csv',index=False)                                     
