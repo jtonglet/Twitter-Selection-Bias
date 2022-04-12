cd3fca679f4bcc5935c808d61bfd99dd8e9c9cb9
Jonathan Tonglet <49620076+jtonglet@users.noreply.github.com>
2022-04-12 14:36:30 +0200

    Add files via upload

---
+++ remove_company_train_set.py
@@ -0,0 +1,17 @@
+#Remove company and organization accounts from the train set as a preprocessing step with M3 predictions
+
+import pandas as pd
+import numpy as np
+
+
+M3_predictions = pd.read_csv('prediction_full_dataset_final.csv')
+train_df = pd.read_csv('output/train_set.csv')
+
+t = 0.95  #Threshold set at 0.95
+company_ids = M3_predictions.apply(lambda row : str(row['id']) if row['org'] >= t else np.nan, axis = 1).dropna().to_list()
+
+company_mask = train_df['user_id'].apply(lambda row : True if str(row) in company_ids else False)
+train_df_no_company = train_df[~company_mask]
+
+train_df_no_company.to_csv('output/train_set_no_company.csv',
+                           index = False)
