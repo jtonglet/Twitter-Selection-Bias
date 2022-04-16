39e28faced2f71c08bcd8f836bba8bb7531aeb93
Jonathan Tonglet <49620076+jtonglet@users.noreply.github.com>
2022-04-16 18:31:31 +0200

    Update remove_company_train_set.py

--- Data_Collection/remove_company_train_set.py
+++ Data_Collection/remove_company_train_set.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 
-M3_predictions = pd.read_csv('prediction_full_dataset_final.csv')
+M3_predictions = pd.read_csv('output/prediction_full_dataset_final.csv')
 train_df = pd.read_csv('output/train_set.csv')
 
 t = 0.95  #Threshold set at 0.95
